b0116462f61d109fe54734457b5c320fa70a9c0c
Julio Rojas <juliorojas81871@gmail.com>
2025-05-09 20:07:04 -0400

    add agent page

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,18 @@
+import Agent from '@/components/Agent';
+import React from 'react'
+
+const Page = async () => {
+    return (
+        <>
+            <h3>Interview generation</h3>
+
+            <Agent
+                userName="You"
+                userId="user1"
+                type="generate"
+            />
+        </>
+    );
+};
+
+export default Page;
